c1cc82c9ff3904c2a4f8f999dd3fb53fabdfaca6
henilkajavadra <henilkajavadra@users.noreply.github.com>
2023-06-23 19:25:57 +0100

    Url bug-test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
-  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }
+  { path: '/', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
+  { path: '/projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }
 ];
 
 @NgModule({
